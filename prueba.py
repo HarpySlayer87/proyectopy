61777129c398ac5ec27d61268cc797163fbe51ac
HarpySlayer87 <hader189@gmail.com>
2023-09-24 11:32:14 -0500

    se agrega los archivos py

---
+++ prueba.py
@@ -0,0 +1,24 @@
+#declaracion de variables
+meses_o_anos=0
+edad=0
+meses=0
+
+
+# leer datos
+
+
+# operaciones
+
+edad = int(input("¿Cuántos años tiene? "))
+if edad < 0:
+    print("No se puede tener una edad negativa")
+if edad >= 0 and edad < 3:
+    print("Valor matricula es $120.000")
+
+if edad >= 4 and edad < 6:
+    print("Valor matricula es $140.000")
+
+if edad >= 7 and edad < 12:
+    print("Valor matricula es $180.000")
+
+#imprimir
